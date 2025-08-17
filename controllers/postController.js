188dfc7e7139f14c819fbf865b2c27a3d7db8b6d
atiya-inayat <atiyainayat869@gmail.com>
2025-08-17 15:27:10 +0500

    improve overall folder structure of backend

---
+++ controllers/postController.js
@@ -0,0 +1,68 @@
+import Post from "../models/Post";
+
+export const createPost = async (req, resizeBy, next) => {
+  // new post
+  try {
+    const { title, description } = req.body; // This means: “Take title and description from the data the user sent to us.”
+    const newPost = await Post.create({ title, description }); // Create a new Post object using the extracted data.
+    res.status(201).json(newPost);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  // find all posts
+  try {
+    // reminder
+    const posts = await Post.find(); // Post.find() → Get all posts from the database.
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getPostsById = async (req, res, next) => {
+  // Find a specific post by id
+  try {
+    const post = await Post.findById(req.params.id); // req.params.id → The id from the URL.
+    if (!post) {
+      return res.status(404).json({ message: "Post not Found" });
+    }
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  // Update or modifiy post
+  try {
+    const { title, description } = req.body;
+    const UpdatedPost = await Post.findByIdAndUpdate(
+      // Whatever the database returns after updating will be stored inside this variable. it will have two parameters: first one will be the id of post which we wanna update , second will be the object(inside it , it will have things we want to update, like title , desc), third one is optional without it it will display old post with it it will replace old with new one
+      req.params.id,
+      { title, description },
+      { new: true, runValidators: true }
+    );
+    if (!UpdatedPost) {
+      return res.status(404).json({ message: "Post not Found" });
+    }
+    res.json(UpdatedPost);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deletePost = async (req, res, next) => {
+  // router.delete → Runs when a DELETE request is sent to this route.
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.id); // Finds the post by ID and deletes it.
+    if (!deletedPost) {
+      return res.status(404).json({ message: "Post not Found" });
+    }
+    // res.json(deletedPost);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,20 +1,42 @@
 import express from "express";
 import Post from "../models/Post.js";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
 // Create a new post
-router.post("/", async (req, res) => {
-  // new post
-  try {
-    const { title, description } = req.body; // This means: “Take title and description from the data the user sent to us.”
-    const newPost = new Post({ title, description }); // Create a new Post object using the extracted data.
-    const savedPost = await newPost.save(); // Store that Post into the database permanently.
-    res.status(201).json(savedPost);
-  } catch (err) {
-    res.status(500).json({ message: "Error Creating POst", error: err });
+router.post(
+  "/",
+  [
+    check("title")
+      .notEmpty()
+      .withMessage("Title is required")
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 character long"),
+    check("description")
+      .notEmpty()
+      .withMessage("Description is required")
+      .isLength({ min: 10 })
+      .withMessage("Description must be at least 10 characters long"),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // new post
+    try {
+      const { title, description } = req.body; // This means: “Take title and description from the data the user sent to us.”
+      const newPost = new Post({ title, description }); // Create a new Post object using the extracted data.
+      const savedPost = await newPost.save(); // Store that Post into the database permanently.
+      res.status(201).json(savedPost);
+    } catch (err) {
+      res.status(500).json({ message: "Error Creating POst", error: err });
+    }
   }
-});
+);
 
 // Get all posts
 router.get("/", async (req, res) => {
@@ -44,24 +66,46 @@ router.get("/:id", async (req, res) => {
 });
 
 // Update post
-router.put("/:id", async (req, res) => {
-  // Update or modifiy post
-  try {
-    const { title, description } = req.body;
-    const UpdatedPost = await Post.findByIdAndUpdate(
-      // Whatever the database returns after updating will be stored inside this variable. it will have two parameters: first one will be the id of post which we wanna update , second will be the object(inside it , it will have things we want to update, like title , desc), third one is optional without it it will display old post with it it will replace old with new one
-      req.params.id,
-      { title, description },
-      { new: true }
-    );
-    if (!UpdatedPost) {
-      return res.status(404).json({ message: "Post not Found" });
+router.put(
+  "/:id",
+
+  [
+    check("title")
+      .notEmpty()
+      .withMessage("Title is required")
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 character long"),
+    check("description")
+      .notEmpty()
+      .withMessage("Description is required")
+      .isLength({ min: 10 })
+      .withMessage("Description must be at least 10 characters long"),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(UpdatedPost);
-  } catch (err) {
-    res.status(500).json({ message: "Error Updating Post", error: err });
+
+    // Update or modifiy post
+    try {
+      const { title, description } = req.body;
+      const UpdatedPost = await Post.findByIdAndUpdate(
+        // Whatever the database returns after updating will be stored inside this variable. it will have two parameters: first one will be the id of post which we wanna update , second will be the object(inside it , it will have things we want to update, like title , desc), third one is optional without it it will display old post with it it will replace old with new one
+        req.params.id,
+        { title, description },
+        { new: true }
+      );
+      if (!UpdatedPost) {
+        return res.status(404).json({ message: "Post not Found" });
+      }
+      res.json(UpdatedPost);
+    } catch (err) {
+      res.status(500).json({ message: "Error Updating Post", error: err });
+    }
   }
-});
+);
 
 // Delete post
 router.delete("/:id", async (req, res) => {
