2d98d3521ec6ced3c5eff284931acbd6bf9107ce
atiya-inayat <atiyainayat869@gmail.com>
2025-08-17 15:59:49 +0500

    --

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import postRoutes from "./routes/postRoutes.js";
+import { notFound, errorHandlerr } from "./middlewares/errorHandlers.js";
 dotenv.config();
 
+// DB + Server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -16,10 +18,14 @@ mongoose
 const app = express();
 app.use(express.json());
 
+// routes
 app.use("/posts", postRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is working...");
 });
+
+// 404 + error handlers (must after routes)
+app.use(notFound);
+app.use(errorHandlerr);
 
 app.listen(5000, () => console.log("🚀 Server running on port 5000"));
